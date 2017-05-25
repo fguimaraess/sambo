4c745f2d6c3e0d8b7c9ed91a21263cf5b2b24094
Juan Pessanha <juanpessanha@gmail.com>
2017-05-25 17:38:17 -0300

    Configura botão cancelar do card de cadastro de socio

--- public/js/socio.js
+++ public/js/socio.js
@@ -6,9 +6,16 @@ var pageSocio = {
     telefoneField: document.querySelector('#telefone-field'),
     dataNascimentoField: document.querySelector('#datanascimento-field'),
     btnSalvar: document.querySelector('#save-card-socio'),
+    btnCancelar: document.querySelector('#cancel-card-socio'),
     bodySocio: document.querySelector('#body-socio')
 }
 window.addEventListener('load', getSocio);
+
+pageSocio.btnCancelar.addEventListener('click', function () {
+    $('.socio-div').show();
+    $('#campos-socio').show();  
+    $('#cardAddsocio').hide();
+})
 
 pageSocio.btnSalvar.addEventListener('click', function () {
     var tempSocio = {
@@ -18,6 +25,7 @@ pageSocio.btnSalvar.addEventListener('click', function () {
         telefone: pageSocio.telefoneField.value,
         datanascimento: pageSocio.dataNascimentoField.value
     };
+
 
     novoSocio(tempSocio);
 })
@@ -31,9 +39,9 @@ function novoSocio(tempSocio) {
     }
 }
 
-function getSocio(){
-    firebase.database().ref('socios/').once('value').then(function (snapshot){
-        snapshot.forEach(function (socioRef){
+function getSocio() {
+    firebase.database().ref('socios/').once('value').then(function (snapshot) {
+        snapshot.forEach(function (socioRef) {
             var tempSocio = socioRef.val();
             tempSocio.uid = socioRef.key;
             preencheTabela(tempSocio);
@@ -41,24 +49,24 @@ function getSocio(){
     })
 }
 
-function preencheTabela(tempSocio){
+function preencheTabela(tempSocio) {
     var html = '';
-    html += '<tr class="sociosTabela" id="'+tempSocio.uid+'">';
-    html += '<td class="cpfSocio">'+tempSocio.cpf+'</td>';
-    html += '<td class="nomeSocio">'+tempSocio.nome+'</td>';
-    html += '<td class="emailSocio">'+tempSocio.email+'</td>';
-    html += '<td class="telefoneSocio">'+tempSocio.telefone+'</td>';
-    html += '<td class="dataSocio">'+tempSocio.datanascimento+'</td>';
+    html += '<tr class="sociosTabela" id="' + tempSocio.uid + '">';
+    html += '<td class="cpfSocio">' + tempSocio.cpf + '</td>';
+    html += '<td class="nomeSocio">' + tempSocio.nome + '</td>';
+    html += '<td class="emailSocio">' + tempSocio.email + '</td>';
+    html += '<td class="telefoneSocio">' + tempSocio.telefone + '</td>';
+    html += '<td class="dataSocio">' + tempSocio.datanascimento + '</td>';
     html += '<td class="dataSocio">XXX</td>';
     html += '<td><a onclick="abreCardSocio(\'' + tempSocio.uid + '\')" href="#" class="editar-socio"><i class="material-icons">mode_edit</i></a>' + '&nbsp;&nbsp;' + '<a onclick="excluirSocio(\'' + tempSocio.uid + '\' )" href="#" class="excluir-socio"><i class="material-icons"><i class="material-icons">remove_circle</i></td>';
     html += '</tr>'
     $('#body-socio').append(html);
 }
 
-function abreCardSocio(idSocio){
+function abreCardSocio(idSocio) {
     console.log(idSocio)
 }
 
-function excluirSocio(idSocio){
+function excluirSocio(idSocio) {
     firebase.database().ref('socios/' + idSocio).remove();
 }
