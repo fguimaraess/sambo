cd6ce227a86fdf0dde8db86e7e5344e971b637f1
Juan Pessanha <juanpessanha@gmail.com>
2017-05-25 14:59:53 -0300

    adiciona método para salvar usuários no banco, e obrigatoriedade de preenchimento

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -4,10 +4,12 @@ var pageDash = {
     btnPgto: document.querySelector("#pagamentos-dashboard"),
     btnRelatorio: document.querySelector("#relatorios-dashboard"),
     btnAddsocio: document.querySelector("#addSocio"),
-    logoSambo: document.querySelector("#logo-sambo")
+    logoSambo: document.querySelector("#logo-sambo"),
+    birthDate: document.querySelector("#datanascimento-field")
 }
 pageDash.btnSocio.addEventListener('click', function(){
     $('.socio-div').show();
+    $('#campos-socio').show();    
     $('.pagamento-div').hide();
     $('.relatorio-div').hide();
     $('#cardAddsocio').hide();
@@ -28,10 +30,40 @@ pageDash.btnRelatorio.addEventListener('click', function(){
 })
 
 pageDash.btnAddsocio.addEventListener('click', function(){
+    $('#campos-socio').hide();
     $('#cardAddsocio').show();
-    $('.socio-div').hide();
 })
 
 pageDash.logoSambo.addEventListener('click', function(){
     window.location = "/dashboard.html";
+})
+
+pageDash.birthDate.addEventListener('click', function(){
+    $('.datepicker').pickadate({
+    selectMonths: true, // Creates a dropdown to control month
+    selectYears: 15, // Creates a dropdown of 15 years to control year
+
+    //altera linguagem do botão de navegação do mês no datepicker
+    labelMonthNext: 'Mês seguinte',
+    labelMonthPrev: 'Mês anterior',
+
+    //altera linguagem da label no dropdown de mês e ano
+    labelMonthSelect: 'Selecione o Mês',
+    labelYearSelect: 'Selecione o Ano',
+
+    //altera linguagem dos dias e meses
+    monthsFull: [ 'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro' ],
+    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+    weekdaysFull: [ 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado' ],
+    weekdaysShort: [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab' ],
+    weekdaysLetter: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S'] ,
+
+    //altera a linguagem dos botões
+    today: 'Hoje',
+    clear: 'Limpar',
+    close: 'Fechar',
+
+    //Formato de data
+    format: 'dd/mm/yyyy'
+ });
 })
--- public/js/socio.js
+++ public/js/socio.js
@@ -1,4 +1,29 @@
 var pageSocio = {
-
+    nomeField: document.querySelector('#nomesocio-field'),
+    cpfField: document.querySelector('#cpf-field'),
+    emailField: document.querySelector('#email-field'),
+    telefoneField: document.querySelector('#telefone-field'),
+    dataNascimentoField: document.querySelector('#datanascimento-field'),
+    btnSalvar: document.querySelector('#save-card-socio')
 }
 
+pageSocio.btnSalvar.addEventListener('click', function () {
+    var tempSocio = {
+        nome: pageSocio.nomeField.value,
+        cpf: pageSocio.cpfField.value,
+        email: pageSocio.emailField.value,
+        telefone: pageSocio.telefoneField.value,
+        datanascimento: pageSocio.dataNascimentoField.value
+    };
+
+    novoSocio(tempSocio);
+})
+
+function novoSocio(tempSocio) {
+    if (tempSocio.nome == "") {
+        swal("", "Verifique os campos obrigatórios", "error")
+    } else {
+        swal("", "Sócio cadastrado com sucessso", "success");
+        firebase.database().ref('socios/').push(tempSocio);
+    }
+}
