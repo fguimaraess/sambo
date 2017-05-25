87702c34523c39855307ceab5487881cf6cef1e6
Juan <juanpessanha@gmail.com>
2017-05-24 22:58:57 -0300

    Adcionado o botão de cadastro (sócios e pagamentos), card de cadstro de sócios e click no logo volta pra home

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -2,23 +2,36 @@ var pageDash = {
     btnUsuario: document.querySelector('#btn-logout'),
     btnSocio: document.querySelector("#socios-dashboard"),
     btnPgto: document.querySelector("#pagamentos-dashboard"),
-    btnRelatorio: document.querySelector("#relatorios-dashboard")
+    btnRelatorio: document.querySelector("#relatorios-dashboard"),
+    btnAddsocio: document.querySelector("#addSocio"),
+    logoSambo: document.querySelector("#logo-sambo")
 }
 pageDash.btnSocio.addEventListener('click', function(){
     $('.socio-div').show();
     $('.pagamento-div').hide();
     $('.relatorio-div').hide();
+    $('#cardAddsocio').hide();
 });
 
 pageDash.btnPgto.addEventListener('click', function(){
     $('.pagamento-div').show();
     $('.relatorio-div').hide();
-    $('.socio-div').hide();    
+    $('.socio-div').hide();  
+    $('#cardAddsocio').hide();  
 });
 
 pageDash.btnRelatorio.addEventListener('click', function(){
     $('.relatorio-div').show();
     $('.pagamento-div').hide();
     $('.socio-div').hide();
+    $('#cardAddsocio').hide();
 })
 
+pageDash.btnAddsocio.addEventListener('click', function(){
+    $('#cardAddsocio').show();
+    $('.socio-div').hide();
+})
+
+pageDash.logoSambo.addEventListener('click', function(){
+    window.location = "/dashboard.html";
+})
