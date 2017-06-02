2982ea76d9dce3a318c1cdbbbc1fc5232a97bd7f
fguimaraess <felipeemg@gmail.com>
2017-06-02 16:47:34 -0300

    Tudo funcionando sem relatorio

--- public/js/relatorio.js
+++ public/js/relatorio.js
@@ -1,5 +1,5 @@
-var pageRelatorio = {
-    socioField: document.querySelector('#socio-combo'),
+/*var pageRelatorio = {
+    socioComboRelatorio: document.querySelector('#socio-combo-relatorio'),
     searchBtn: document.querySelector('#search-btn'),
     relatorioSideBtn: document.querySelector('#relatorios-dashboard')
 }
@@ -10,11 +10,11 @@ pageRelatorio.searchBtn.addEventListener('click', function () {
 })
 
 function getSociosCombo(){
-    $(pageRelatorio.socioField).empty();
+    $(pageRelatorio.socioComboRelatorio).empty();
     var newOption = document.createElement("option");
     newOption.value = "-";
     newOption.innerHTML = "-";
-    pageRelatorio.socioField.options.add(newOption);
+    pageRelatorio.socioComboRelatorio.options.add(newOption);
     var tempSocio = [];
     tempSocio = pageSocio.socios;
     for (var key in tempSocio){
@@ -28,5 +28,5 @@ function preencheComboSocio(tempSocio){
     var newOption = document.createElement("option");
     newOption.value = tempSocio.uid;
     newOption.innerHTML = tempSocio.nome;
-    pageRelatorio.socioField.options.add(newOption);
-}
+    pageRelatorio.socioComboRelatorio.options.add(newOption);
+}*/
